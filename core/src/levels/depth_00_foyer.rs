3544f1a8d7e361b96b8c3e365b1f9871a6aae6ef
ThatXliner <bryan.hu.2020@gmail.com>
2026-06-11 00:35:51 -0700

    Update depth_00_foyer.rs

--- core/src/levels/depth_00_foyer.rs
+++ core/src/levels/depth_00_foyer.rs
@@ -34,7 +34,7 @@ pub(crate) fn build_foyer(world: &mut World) {
     // Single sign: boot, controls, wizard tease
     world.ecs.spawn_sign(
         Position::new(rx + rw / 2 - 6, ry + rh / 2 - 1),
-        "If you're reading this, you finally woke up.\n\nMove with arrow keys or hjkl. The stairs down are in the far corner.\n\nThat green thing is a slime. It wants what you want.",
+        "If you're reading this, you finally woke up.\n\nMove with arrow keys or hjkl. The stairs down are in the far corner.\n\nThat (S) is a slime. It wants to jump on you.",
     );
 
     // One pushable slime
